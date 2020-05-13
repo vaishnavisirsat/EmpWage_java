110dce5ee48d07b2561a565e3ff0c9fc04f256f9
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-05-13 16:58:10 +0530

    Checking employee attendance

---
+++ EmpWageComputation.java
@@ -0,0 +1,13 @@
+class EmpWageComputation {
+	public static void main(String args[]) {
+		// CONSTANTS
+		int IS_FULL_TIME = 1;
+		//COMPUTATION
+		int empCheck = (int) ( Math.random() * 10 ) % 2;
+		System.out.println(empCheck);
+		if ( empCheck == IS_FULL_TIME )
+			System.out.println( "Emplyee is Present" );
+		else
+			System.out.println( "Employee is Absent" ); 
+	}
+}
