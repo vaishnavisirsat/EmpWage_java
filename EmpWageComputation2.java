0b8b5665f2c719449b269e2656d9cbf3885af509
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-05-13 20:23:51 +0530

    Calculatr employee wage

---
+++ EmpWageComputation2.java
@@ -0,0 +1,20 @@
+class EmpWageComputation2 {
+
+	public static void main(String args[]) {
+		// CONSTANTS
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		//VARIABLES
+		int empHrs = 0;
+		int empWage = 0;
+		//COMPUTATION
+		int empCheck = (int) ( Math.random() * 10 ) % 2;
+		if ( empCheck == IS_FULL_TIME )
+
+			empHrs = 8;
+		else 
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
+	}
+}
