d3aaa8f3f894b25423e1f16c8844f90f82bacc4e
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-05-13 20:33:28 +0530

    Add full time using switch case

---
+++ EmpWageComputation4.java
@@ -0,0 +1,25 @@
+public class EmpWageComputation4 {
+	//CONSTANTS
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static void main(String args[]) {
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck)	{
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
+	}
+
+}
+
