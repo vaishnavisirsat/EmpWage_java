7f9a2df6ca13e2cdfe46b68448a2a1a11b59f585
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-05-13 20:53:31 +0530

    Use class method to compute Employee wage

---
+++ EmpWageComputation7.java
@@ -0,0 +1,36 @@
+public class EmpWageComputation7 {
+	//CONSTANTS
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
+ 	public static int calTotalEmpWage( int totalEmpHrs )	{
+		return totalEmpHrs * EMP_RATE_PER_HOUR;
+	}
+
+ 	public static void main(String args[]) {
+		//VARIABLES
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		//COMPUTATION
+		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS )
+		{
+			totalWorkingDays++;
+			int empCheck = (int) ( Math.random() * 10 ) % 3;
+			switch (empCheck)
+			{
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
+		}
+		int totalEmpWage = calTotalEmpWage( totalEmpHrs );
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+
+	}
+}
